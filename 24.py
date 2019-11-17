009c0c6e7f652444ed0d4774d98b1eaea3d016bc
hoonihu1 <hoonihu@gmail.com>
2019-11-17 17:49:17 +0900

    python_ta_2

--- 24.py
+++ 24.py
@@ -1,8 +1,29 @@
+# import re
+#
+# var = input()
+#
+# var2 = re.compile('the[^a-zA-Z]')
+# var2 = var2.findall(var)
+#
+# print(len(var2))
+
+import csv
 import re
 
-var = input()
+a = ''
 
-var2 = re.compile(r'the[^a-zA-Z]')
-var2 = var2.findall(var)
+with open('python_input.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        a += ''.join(row)
 
-print(len(var2))
+p = re.compile('\d{3}-\d{4}-\d{4}')
+l = re.compile('\d{3}-\d{4}-\d{4}|\d{3}-\d{3}-\d{4}')
+n = re.compile('\d{3}\.\d{4}\.\d{4}')
+m = p.findall(a)
+k = l.findall(a)
+g = n.findall(a)
+
+print(m)
+print(k)
+print(g)
